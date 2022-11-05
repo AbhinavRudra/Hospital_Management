ca07d2051acf6913d85b60fc6c8ce07aea4868ce
Yin Yang <84718589+Yinyang005@users.noreply.github.com>
2022-11-05 21:44:40 +0530

    Update tablecreation.py

--- tablecreation.py
+++ tablecreation.py
@@ -1,30 +1,26 @@
-import mysql.connector as sql
-conn = sql.connect(
-    host='localhost',
-    user='root',
-    password='root',
-    database='Hospital_Database',
-    auth_plugin='mysql_native_password'
-)
-c1 = conn.cursor()
-
-c1.execute('''CREATE TABLE Patient_Details(
-    Patient_Name varchar(20) Primary Key, 
-    Patient_Age int(10), 
-    Patient_Disease_Illness varchar(100), 
-    Phone_no int(10))
-    ''')
-
-c1.execute('''CREATE TABLE Doctor_Details(
-    Doctor_Name varchar(20) Primary Key,
-    Doctor_Age int,
-    Doctor_Department varchar(100),
-    Phone_no int(10))
-    ''')
-
-c1.execute('''CREATE TABLE Worker_Details(
-    Worker_Name varchar(20) Primary Key,
-    Worker_Age int,
-    Work_Description varchar(100),
-    Phone_no int(10))
-    ''')
+import mysql.connector as sql 
+conn = sql.connect(     # handled by Aakash 
+    host='localhost', 
+    user='root', 
+    password='root', 
+    database='Aster_Database' ) 
+c1 = conn.cursor() 
+ 
+c1.execute('''CREATE TABLE Patient_Details(   # handled by Aakash 
+    Patient_Name varchar(20) Primary Key,  
+    Patient_Age int(10),  
+    Patient_Problems varchar(100),  
+    Phone_no int(10)) 
+    ''') 
+c1.execute('''CREATE TABLE Doctor_Details(   # handled by Gowri 
+    Doctor_Name varchar(20) Primary Key, 
+    Doctor_Age int, 
+    Doctor_Department varchar(100), 
+    Phone_no int(10)) 
+    ''') 
+c1.execute('''CREATE TABLE Staff_Details(   # handled by Nipun 
+    Staff_Name varchar(20) Primary Key, 
+    Staff_Age int, 
+    Staff_Workname varchar(100), 
+    Phone_no int(10)) 
+    ''') 
